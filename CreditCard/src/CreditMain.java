bfd6e44b228f8f739b118e6bd8b8fbaf61c52d47
maxt.obrecht <maxt.obrecht@localhost>
2024-02-13 20:47:42 -0700

    Code produces random 16 digits, still trying to remove cards that don't fir the algorithm

--- CreditCard/src/CreditMain.java
+++ CreditCard/src/CreditMain.java
@@ -3,13 +3,31 @@ import java.util.Scanner;
 
 public class CreditMain
 	{
+		static ArrayList<Long> aList = new ArrayList<Long>();
 
 		public static void main(String[] args)
 			{
-				long fullNumber = creditCardNumber();
-				long[] unchangedNum = loadArray(fullNumber);
-				long[] doubledArray = doubledArray(unchangedNum);
-				addDigits(doubledArray);
+				int count = 0;
+				while (count < 100)
+					{
+						long fullNumber = randomCreditCardNumber();
+						long[] unchangedNum = loadArray(fullNumber);
+						long[] doubledArray = doubledArray(unchangedNum);
+						long[] addedDigits = addDigits(doubledArray);
+						divisibleByTen(addedDigits);
+						boolean list = list(addedDigits);
+
+						if (list)
+							{
+								count++;
+							}
+					}
+				
+//				long fullNumber = creditCardNumber();
+//				long[] unchangedNum = loadArray(fullNumber);
+//				long[] doubledArray = doubledArray(unchangedNum);
+//				long[] addedDigits = addDigits(doubledArray);
+//				divisibleByTen(addedDigits);
 
 			}
 
@@ -23,6 +41,21 @@ public class CreditMain
 				return num;
 			}
 
+		public static long randomCreditCardNumber()
+			{
+				long count = 1;
+				long num = 0;
+				for (int i = 0; i < 16; i++)
+					{
+						long digit = ((int) (Math.random() * 9) + 1) * count;
+						digit--;
+						num += digit;
+						count *= 10;
+					}
+				aList.add(num);
+				return num;
+			}
+
 		public static long[] loadArray(long num)
 			{
 				long[] localArray = new long[16];
@@ -30,14 +63,14 @@ public class CreditMain
 					{
 						localArray[i] = num % 10;
 						num /= 10;
-						// System.out.println(a[i]);
+						// System.out.println(localArray[i]);
 					}
 				return localArray;
 			}
 
 		public static long[] doubledArray(long[] num)
 			{
-				for (int i = 0; i < 16; i++)
+				for (int i = 0; i < 16; i += 2)
 					{
 						num[i] = (num[i] * 2);
 					}
@@ -50,14 +83,48 @@ public class CreditMain
 					{
 						if (num[i] / 10 > 0)
 							{
-								int firstDigit = (int) (num[i]%10);
-								num[i]/=10;
-								num[i]+=firstDigit;
-								//System.out.println(num[i]);
-								
+								int firstDigit = (int) (num[i] % 10);
+								num[i] /= 10;
+								num[i] += firstDigit;
+								// System.out.println(num[i]);
 							}
 					}
 				return num;
+			}
+
+		public static long[] divisibleByTen(long[] num)
+			{
+				int sum = 0;
+				for (int i = 15; i >= 0; i--)
+					{
+						sum += num[i];
+						//System.out.println("sum is " + sum);
+					}
+
+				if (sum % 10 == 0)
+					{
+						System.out.println("works");
+						return num;
+					}
+				else
+					{
+						System.out.println("not");
+						return null;
+					}
+			}
+
+		public static boolean list(long[] num)
+			{
+				if (num != null)
+					{
+						System.out.println(aList.get(aList.size() - 1));
+						return true;
+					}
+				else
+					{
+						aList.remove(aList.size());
+						return false;
+					}
 			}
 
 	}
